c2d029895448746739e708d59a6270056770af52
OG-Design <marcus@olsen-graphics.no>
2026-01-30 00:29:46 +0100

    Fixed .env error not reaching api in frontend

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig(({mode})=>{
   const env = loadEnv(mode, process.cwd()+"../");
-  const API_URL = env.API_URL || "" ; // FIX!
+  const API_URL = env.API_URL || "http://localhost:3000" ; // Double check
   return {
     plugins: [vue()],
     server: {
@@ -12,12 +12,12 @@ export default defineConfig(({mode})=>{
       host: '0.0.0.0',
       proxy: {
         '/api': {
-          API_URL,
+          target: API_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, '')
         },
         '/socket.io': {
-        API_URL,
+        target: API_URL,
         ws: true,
         changeOrigin: true
         }
